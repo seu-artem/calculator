ca4a88e4711d3aaecb340b88c4d0ff3981b30485
Usyk Artem <usyk1949@gmail.com>
2023-10-30 02:14:14 +0200

    added 5-15 lines

--- calculator.cpp
+++ calculator.cpp
@@ -7,10 +7,15 @@ int Calculator::Add (double a, double b)
 
 int Calculator::Sub (double a, double b)
 {
-    return Add (a, -b);
+  return Add (a, -b);
 }
 
 int Calculator::Mul (double a, double b)
 {
-    return a * b + 0.5;
+	return a * b;
+}
+
+int Calculator::Div (double a, double b)
+{
+	return a / b;
 }
